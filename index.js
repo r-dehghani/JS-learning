3f03e3e5a172fe29090827405cd0845fe54687cd
dariush dehghani <r.dehghani.90@gmail.com>
2025-08-22 14:12:44 +0330

    learning JS-1

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log("dariush")
+
+let namee = "mamad"
+// let name = "Dariush Dehghani"
+let neme_family
+let name$family
+let name2family 
+const PI = 3.14
+// PI = 2.14
+console.log(PI)
+var checked = true
+let num = -12
+let count = 23
+let other_num = 0.14
+let base = 070
+console.log(base)
+let message = "Hello world";
+checked = false
+console.log(checked)
+console.log(typeof checked)
+
+let person = {
+    name:"mansour",
+    age:36,
+}
+console.log(person.name, person.age)
+console.log(person)
+console.log(typeof person)
+console.log(person["name"],"and",  person["age"])
+
+let thekey = "family"
+person.thekey = "dehghani"
+console.log(person)
+
+let selectedcolor = ['red', "green", "blue"]
+console.log(selectedcolor);
+console.log(selectedcolor[0]);
+console.log(selectedcolor[1]);
+console.log(selectedcolor[2]);
+selectedcolor[3] = "yellow"
+console.log(selectedcolor[3])
+selectedcolor[3] = {
+    name:"dariush",
+    age:29,
+};
+console.log(selectedcolor)
+
+namee = "maamaad";
+console.log(namee)
+function greeting(namee){
+    console.log("hello " + namee);
+};
+
+greeting(namee);
